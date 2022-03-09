7afd50eb720380511527274f135c64952fd49226
Vasilii Lysenkov <vlysenkov@griddynamics.com>
2022-03-09 21:53:17 +0300

    added valid info

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,6 +1,11 @@
 package ru.netology.data;
 
+import com.github.javafaker.Faker;
 import lombok.Value;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
 
 public class DataHelper {
     @Value
@@ -10,5 +15,28 @@ public class DataHelper {
         private String year;
         private String owner;
         private String cvv;
+    }
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    public static String getRandomMonth() {
+        return faker.number().numberBetween(1, 13) + "";
+    }
+    public static String getRandomYear() {
+        LocalDate localDate = LocalDate.now();
+        String year = (localDate.getYear() + "").substring(2);
+        return faker.number().numberBetween(Integer.parseInt(year), (Integer.parseInt(year) + 6)) + "";
+    }
+    public static String getRandomHolderName() {
+        return faker.name().fullName();
+    }
+    public static String getRandomCvc() {
+        return faker.number().digits(3);
+    }
+    public static AuthInfo getApprovedCard() {
+        return new AuthInfo("4444 4444 4444 4441", getRandomMonth(), getRandomYear(), getRandomHolderName(), getRandomCvc());
+    }
+    public static AuthInfo getDeclinedCard() {
+        return new AuthInfo("4444 4444 4444 4442", getRandomMonth(), getRandomYear(), getRandomHolderName(), getRandomCvc());
     }
 }
